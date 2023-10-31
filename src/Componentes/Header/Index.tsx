e58440e6cb5bd1a8bbcc5fd21dfcc7373e9cf7cc
leandrocha <91626637+leandrocha@users.noreply.github.com>
2023-10-31 15:20:50 -0300

    TESTE

--- src/Componentes/Header/Index.tsx
+++ src/Componentes/Header/Index.tsx
@@ -2,7 +2,7 @@ import style from '../Header/Header.module.scss'
 
 export default function Header() {
     return <div className={style.HeaderApp}>
-      <h1 className={style.HeaderAppTitulo}>To Do App</h1>
+      <h1 className={style.HeaderAppTitulo}>To Do APP</h1>
       <h3>By Leandro Rocha</h3>
     </div>
 }
